35390e29b2c913eeb91f61ddeccfb268c1a5a782
Vinicius Bussolo <vinicius.buss13@gmail.com>
2024-02-20 10:00:23 -0300

    :sparkles: feat: aula 02

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Toaster } from 'sonner'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-slate-900 text-slate-50 antialiased`}
       >
+        <Toaster richColors />
         {children}
       </body>
     </html>
